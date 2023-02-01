87b34e5e76321f25dbd0ae13785b85e19d28a43a
Abdul-Rahaman Yussif <abdulrahamanyussif62@gmail.com>
2023-02-01 12:38:20 +0000

    b

--- letsLearn/main.cpp
+++ letsLearn/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-/*using namespace std;
+using namespace std;
 
 int main()
 {
@@ -9,5 +9,3 @@ int main()
     cout << "Welcome to repo";
     return 0;
 }
-*/
-cout << "Hello World";
